7d91c224ae58b09a000c619775c652ac9b9ad63d
Vladimir Pavlyukov <yahusim@mail.ru>
2022-09-21 19:03:55 +0300

    to review

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,8 @@
 package ru.netology;
 
 public class Main {
+    // не запускать, проект только для демонстрации junit тестов
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Не нужно было запускать, проект только для демонстрации junit тестов");
     }
 }
--- src/test/java/ru/netology/BonusServiceTest.java
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -2,7 +2,6 @@ package ru.netology;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,8 +14,10 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class BonusServiceTest {
     private BonusService service;
 
-    /** Метод генерирует последовательность тестовых данных,
-     * <p> для параметрического теста */
+    /**
+     * Метод генерирует последовательность тестовых данных,
+     * <p> для параметрического теста
+     */
     public static Stream<Arguments> testDataProvider() {
         return Stream.of(
                 arguments("Нижняя граница, для зарегистрированных пользователей", 0L, true, 0L),
@@ -27,25 +28,26 @@ class BonusServiceTest {
                 arguments("Ниже верхней границы, для зарегистрированных пользователей", 16_666L, true, 499L),
                 arguments("Выше верхней границы, для зарегистрированных пользователей", 16_667L, true, 500L),
 
+
                 arguments("Нижняя граница, для не зарегистрированных пользователей", 0L, false, 0L),
                 arguments("Нижняя граница + 1, для не зарегистрированных пользователей", 1L, false, 0L),
 
-                arguments("Значение из интервала, для не зарегистрированных пользователей", 10_000L, true, 100L),
+                arguments("Значение из интервала, для не зарегистрированных пользователей", 10_000L, false, 100L),
 
-                arguments("Верхняя граница - 1, для не зарегистрированных пользователей", 49_999L, false, 500L),
+                arguments("Верхняя граница - 1, для не зарегистрированных пользователей", 49_999L, false, 499L),
                 arguments("Верхняя граница, для не зарегистрированных пользователей", 50_000L, false, 500L),
                 arguments("Верхняя граница + 1, для не зарегистрированных пользователей", 50_001L, false, 500L)
         );
     }
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         // перед каждым тестом создаем тестируемый объект
         service = new BonusService();
     }
 
-    @DisplayName("Параметризованный тест")
-    @ParameterizedTest(name = "{index} - {0} registered is {2}, amount is {1}")
+    @DisplayName("Параметризованный тест расчета бонусов для зарегистрированных и не зарегистрированных пользователей")
+    @ParameterizedTest(name = "{index} - {0}, регистрация {2}, сумма {1}")
     @MethodSource("testDataProvider")
     void test(String description, long amount, boolean isRegistered, long expected) {
 
